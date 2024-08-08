1cd69e7896058acb68dde43ee15469683d36a7bb
yassir2222 <yassir.lambrass@gmail.com>
2024-08-08 16:09:53 +0100

    change

--- article-page/single-pages/epaule/header-epaule.js
+++ article-page/single-pages/epaule/header-epaule.js
@@ -1,9 +1,9 @@
 let head=document.getElementById("head");
 let str=`<nav class="s-header__nav-wrap">
-        <h3 class="s-header__nav-heading">Navigate to</h3>
+        <h3 class="s-header__nav-heading">Naviguer vers</h3>
     
         <ul class="s-header__nav">
-            <li class="current-menu-item"><a href="../../index.html" title="">Home</a></li>
+            <li class="current-menu-item"><a href="../../index.html" title="">Accueil</a></li>
             <li class="has-children">
                 <a href="#0" title="" class="">Épaule</a>
                 <ul class="sub-menu">
@@ -16,58 +16,57 @@ let str=`<nav class="s-header__nav-wrap">
             <li class="has-children">
                 <a href="#0" title="" class="">Main</a>
                 <ul class="sub-menu">
-                    <li><a href="../main/prothese-de-pouce.html">prothése de pouce</a></li>
-                    <li><a href="category.html">ENDOSCOPIE DU CANAL CARPIEN</a></li>
-                    <li><a href="category.html">CHIRURGIE DE LA MALADIE DE DUPUYTREN</a></li>
-                    <li><a href="category.html">CHIRURGIE DE LA TENDINITE DE QUERVAIN</a></li>
-                    <li><a href="category.html">CHIRURGIE DU DOIGT À RESSAUT</a></li>
-                    <li><a href="category.html">CHIRURGIE DU KYSTE SYNOVIAL</a></li>
+                    <li><a href="../main/prothese-de-pouce.html">Prothèse de Pouce</a></li>
+                    <li><a href="../main/endoscopie-canal-carpien.html">Endoscopie Du Canal Carpien</a></li>
+                    <li><a href="../main/maladie-de-dupytren.html"> Chirurgie De La Maladie De Dupuytren</a></li>
+                    <li><a href="../main/tendinite-de-quervain.html">Chirurgie De La Tendinite De Quervain</a></li>
+                    <li><a href="../main/doigt-a-ressaut.html">Chirurgie Du Doigt À Ressaut</a></li>
+                    <li><a href="../main/kyste-synovial.html">Chirurgie Du Kyste Synovial</a></li>
                 </ul>
             </li>
             <li class="has-children">
-                <a href="#0" title="" class="">COLONNE VERTÉBRALE</a>
+                <a href="#0" title="" class="">Colonne Vertébrale</a>
                 <ul class="sub-menu">
-                    <li><a href="category.html">NÉVRALGIE CERVICO-BRACHIALE</a></li>
-                    <li><a href="category.html">ENDOSCOPIE DU CANAL CARPIEN</a></li>
-                    <li><a href="category.html">MYÉLOPATHIE CERVICO-ARTHROSIQUE</a></li>
-                    <li><a href="category.html">CANAL CERVICAL ÉTROIT</a></li>
-                    <li><a href="category.html">CHIRURGIE DU CANAL LOMBAIRE ÉTROIT</a></li>
+                    <li><a href="../colonne-vertebrale/Nevralgie-Cervico-Brachiale.html">Névralgie Cervico-Brachiale</a></li>
+                    <li><a href="../colonne-vertebrale/myelopathie-cervico-arthrosique.html"> Myélopathie Cervico-Arthrosique</a></li>
+                    <li><a href="../colonne-vertebrale/canal-cervical-Arthosique.html">Canal Cervical Étroit</a></li>
+                    <li><a href="../colonne-vertebrale/chirurgie-du-canal-lombaire.html">Chirurgie Du Canal Lombaire Étroit</a></li>
                 </ul>
             </li>
     
             <li class="has-children">
-                <a href="#0" title="" class="">HANCHE</a>
+                <a href="#0" title="" class="">Hanche</a>
                 <ul class="sub-menu">
-                    <li><a href="category.html">LA PROTHÈSE TOTALE DE LA HANCHE</a></li>
-                    <li><a href="category.html">PROTHÈSE DE LA HANCHE PAR VOIE ANTÉRIEURE MINI-INVASIVE</a></li>
-                    <li><a href="category.html">CHIRURGIE DU LABRUM SOUSARTHROSCOPIE DE HANCHE</a></li>
-                    <li><a href="category.html">CHIRURGIE DU CONFLIT FEMORO-ACÉTABULAIRE DE HANCHE SOUSARTHROSCOPIE</a></li>
+                    <li><a href="category.html">La Prothèse Totale De La Hanche</a></li>
+                    <li><a href="category.html">Prothèse De La Hanche Par Voie Antérieure Mini-Invasive</a></li>
+                    <li><a href="category.html">Chirurgie Du Labrum Sousarthroscopie De Hanche</a></li>
+                    <li><a href="category.html">Chirurgie Du Conflit Femoro-Acétabulaire De Hanche Sousarthroscopie</a></li>
                 </ul>
             </li>
-            
+
             <li class="has-children">
-                <a href="#0" title="" class="">GENOU</a>
+                <a href="#0" title="" class="">Genou</a>
                 <ul class="sub-menu">
-                    <li><a href="category.html">ANATOMIE DU GENOU</a></li>
-                    <li><a href="category.html">ARTHROSE DU GENOU OU GONARTHROSE</a></li>
-                    <li><a href="category.html">PATHOLOGIES LIGAMENTAIRES</a></li>
-                    <li><a href="category.html">PATHOLOGIES MÉNISCALES</a></li>
-                </ul>
-            </li>
-            <li class="has-children">
-                <a href="#0" title="" class="">CHEVILLE-PIED</a>
-                <ul class="sub-menu">
-                    <li><a href="category.html">CHIRURGIE DE LA CHEVILLE</a></li>
-                    <li><a href="category.html">PIED DUCHIRURGIE</a></li>
+                    <li><a href="category.html">Anatomie Du Genou</a></li>
+                    <li><a href="category.html">Arthrose Du Genou Ou Gonarthrose</a></li>
+                    <li><a href="category.html">Pathologies Ligamentaires</a></li>
+                    <li><a href="category.html">Pathologies Méniscales</a></li>
                 </ul>
             </li>
             <li class="has-children">
-                <a href="#0" title="" class="">SPORT DUCHIRURGIE</a>
+                <a href="#0" title="" class="">Cheville-Pied</a>
                 <ul class="sub-menu">
-                    <li><a href="category.html">CHEVILLE</a></li>
-                    <li><a href="category.html">ÉPAULE</a></li>
-                    <li><a href="category.html">GENOU</a></li>
-                    <li><a href="category.html">HANCHE</a></li>
+                    <li><a href="category.html">Chirurgie De La Cheville</a></li>
+                    <li><a href="category.html">Pied Duchirurgie</a></li>
+                </ul>
+            </li>
+            <li class="has-children">
+                <a href="#0" title="" class="">Sport Duchirurgie</a>
+                <ul class="sub-menu">
+                    <li><a href="category.html">Cheville</a></li>
+                    <li><a href="category.html">Épaule</a></li>
+                    <li><a href="category.html">Genou</a></li>
+                    <li><a href="category.html">Hanche</a></li>
                 </ul>
             </li>
             <li class="has-children">
